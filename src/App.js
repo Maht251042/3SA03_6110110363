2a17965ee79f4a25908a66275df479f7ca3f26de
Muhammad Waedeng <maht@Muhammads-MacBook-Pro.local>
2020-08-14 19:05:29 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
 import './App.css';
+import WordCard from './WordCard';
 
+const word = "Hello";
 function App() {
   return (
     <div>
-      Hello World
+      {
+       <WordCard value={word}/>
+      }
     </div>
   );
 }
